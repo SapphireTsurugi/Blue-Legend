f1874ceab67bac5d38a82ac0f01cff7716d545a2
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-04 18:25:41 +0530

    Rename BASE.py to ADMINS.py

---
+++ cogs/ADMINS.py
@@ -0,0 +1,35 @@
+import discord,json,os,asyncio,psycopg2
+from discord.ext.commands import *
+from discord_components import Button
+
+con = psycopg2.connect(os.environ.get("DATABASE_URL"))
+cur = con.cursor()
+
+class BASE(Cog):
+
+    def __init__(self,client):
+
+        self.client = client
+
+    @command()
+    async def sql(self,ctx,*code):
+        
+        sqlstr = r" ".join(code)
+        cur.execute(sqlstr)
+        con.commit()
+        await ctx.send("Done ig")
+        
+    @command()
+    async def sqlview(self,ctx,*code):
+        
+        sqlstr = r" ".join(code)
+        cur.execute(sqlstr)
+        data = cur.fetchall()
+        msg = ""
+        for i in data:
+            msg+=str(i)
+            msg+="\n"
+        await ctx.send(msg)
+
+def setup(client):
+    client.add_cog(BASE(client))
