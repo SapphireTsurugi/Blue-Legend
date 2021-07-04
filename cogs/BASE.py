7551f76cd77382e5f02c9517c005c26e170d51c4
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-04 11:27:58 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -1,8 +1,10 @@
 import discord,json,os,asyncio,psycopg2
 from discord.ext.commands import *
 from discord_components import Button
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 con = psycopg2.connect(os.environ.get("DATABASE_URL"))
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = con.cursor()
 
 class BASE(Cog):
