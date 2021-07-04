fc9571d4e9c95387d1aa4c3e87c78e3802a58e33
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-04 09:21:28 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -1,6 +1,9 @@
-import discord,json,os,asyncio
+import discord,json,os,asyncio,psycopg2
 from discord.ext.commands import *
-from discord_components import Button 
+from discord_components import Button
+
+con = psycopg2.connect(os.environ.get("DATABASE_URL"))
+cur = con.cursor()
 
 class BASE(Cog):
 
@@ -9,9 +12,14 @@ class BASE(Cog):
         self.client = client
 
     @command()
-    async def start(self,ctx):
+    async def sql(self,ctx,*code):
         
-        return
+        sqlstr = " ".join(code)
+        cur.execute(sqlstr)
+        con.commit()
+        await ctx.send("Done ig")
+
+con.close()
 
 def setup(client):
     client.add_cog(BASE(client))
