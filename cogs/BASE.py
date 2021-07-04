242d9250c6d60aaced33b61a8d5ce797bfc5302a
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-04 19:06:33 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -1,1 +1,22 @@
-pass
+import discord,json,os,asyncio,psycopg2
+from discord.ext.commands import *
+from discord_components import Button
+
+con = psycopg2.connect(os.environ.get("DATABASE_URL"))
+cur = con.cursor()
+
+class BASE(Cog):
+
+    def __init__(self,client):
+
+        self.client = client
+
+    @command()
+    async def start(self,ctx):
+        
+        
+        cur.execute(f"SELECT * FROM Main WHERE ID = {ctx.author.id}")
+        
+
+def setup(client):
+    client.add_cog(BASE(client))
