ba5b6b65e7ea6cc9d3ca17c98ff07fcfae13d63e
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-05 12:39:56 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -18,9 +18,21 @@ class BASE(Cog):
         if cur.rowcount == 0:
             cur.execute(f"INSERT INTO Main (ID,HP,MHP,DEF,ATK,ARMOR,WEP,LEVEL,XP,LOCX,LOCY,LOCN,LOCZ,GOLD,GEMS,INV,PRESTIGE,BATTLE,TUT_STATE) VALUES ({ctx.author.id},100,100,10,10,'None','None',1,0,1,1,'The Village',1,0,100,'[]',0,0,0)")
             con.commit()
-            await ctx.send("Umm Hello Adventurer. What Can I help you with today? Oh you want to register yourself as a new adventurer? Great. ... . Done!.Type 1tut for the Master to help you. Also Come back after you received the Master's certification to start getting Quests. Good Luck!.")
+            msg = await ctx.send("Umm Hello Adventurer. What Can I help you with today?")
+            await asyncio.sleep(1)
+            await msg.edit("Oh you want to register yourself as a new adventurer? Great.")
+            await asyncio.sleep(5)
+            await msg.edit("Done!.Type 1tut for the Master to help you.")
+            await asyncio.sleep(2)
+            await msg.edit("Also Come back after you received the Master's certification to start getting Quests. Good Luck!.")
         else:
-            await ctx.send("Umm Hello Adventurer. What Can I help you with today? Oh you want to register yourself as a new adventurer? Great. ... . You are a popular adventurer throughout the Kingdom and you want to start over? Maybe are you looking to prestige. Try 1prestige for that. Hope we can meet again soon.")
+            msg = await ctx.send("Umm Hello Adventurer. What Can I help you with today?")
+            await asyncio.sleep(1)
+            await msg.edit("Oh you want to register yourself as a new adventurer? Great.")
+            await asyncio.sleep(5)
+            await msg.edit("You are a popular adventurer throughout the Kingdom and you want to start over?")
+            await asyncio.sleep(2)
+            await msg.edit("Maybe are you looking to prestige. Try 1prestige for that. Hope we can meet again soon.")
         
 
 def setup(client):
