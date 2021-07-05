1ed70165054616019fc3a543bdeee4597113c202
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-05 12:36:04 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -16,7 +16,7 @@ class BASE(Cog):
         
         cur.execute(f"SELECT * FROM Main WHERE ID = {ctx.author.id}")
         if cur.rowcount == 0:
-            cur.execute(f"INSERT INTO Main (ID,HP,MHP,DEF,ATK,ARMOR,WEP,LEVEL,XP,LOCX,LOCY,LOCN,LOCZ,GOLD,GEMS,INV,PRESTIGE,BATTLE,TUT_STATE) VALUES ({ctx.author.id},100,100,10,'None','None',1,0,1,1,'The Village',1,0,100,'[]',0,0,0)")
+            cur.execute(f"INSERT INTO Main (ID,HP,MHP,DEF,ATK,ARMOR,WEP,LEVEL,XP,LOCX,LOCY,LOCN,LOCZ,GOLD,GEMS,INV,PRESTIGE,BATTLE,TUT_STATE) VALUES ({ctx.author.id},100,100,10,10,'None','None',1,0,1,1,'The Village',1,0,100,'[]',0,0,0)")
             con.commit()
             await ctx.send("Umm Hello Adventurer. What Can I help you with today? Oh you want to register yourself as a new adventurer? Great. ... . Done!.Type 1tut for the Master to help you. Also Come back after you received the Master's certification to start getting Quests. Good Luck!.")
         else:
