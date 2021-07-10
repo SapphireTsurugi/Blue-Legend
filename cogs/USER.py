02941fb0c0b405297c949488f0bc7394f7fdc929
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-10 21:16:10 +0530

    update USER.py

--- cogs/USER.py
+++ cogs/USER.py
@@ -17,7 +17,7 @@ class BASE(Cog):
         cur.execute("ROLLBACK")
         cur.execute(f"SELECT * FROM Main WHERE ID = {ctx.author.id}")
         if cur.rowcount == 0:
-            cur.execute(f"INSERT INTO Main (ID,MONEY,LEVEL,XP,BUFFS,WORKPLACE,WTIER,INCOME,LASTWORKED,STAMINA,MSTAMINA,SLEPT,HOUSE,HOUSESOWNED,FOODSOWNED,WSTREAK,DSTREAK,DAILY,DAILYELAPSED) VALUES ({ctx.author.id},0,1,0,'[]','Fast Food',1,10,5,10,10,True,'Homeless','[]','[]',0,0,False,0);")
+            cur.execute(f"INSERT INTO Main (ID,MONEY,LEVEL,XP,BUFFS,WORKPLACE,WTIER,INCOME,LASTWORKED,STAMINA,MSTAMINA,SLEPT,HOUSE,HOUSESOWNED,FOODSOWNED,WSTREAK,DSTREAK,DAILY,DAILYELAPSED,VOTE,VSTREAK) VALUES ({ctx.author.id},0,1,0,'[]','Fast Food',1,10,5,10,10,True,'Homeless','[]','[]',0,0,False,0,False,0);")
             con.commit()
             embed = discord.Embed(title = "Welcome", color = discord.Color.blue())
             embed.set_author(name = ctx.author)
@@ -29,20 +29,27 @@ class BASE(Cog):
             embed = discord.Embed(title = "Welcome", color = discord.Color.blue())
             embed.set_author(name = ctx.author)
             embed.set_thumbnail(url = ctx.author.avatar_url)
-            embed.add_field(name = "Umm Hello Adventurer. What Can I help you with today?",value = "Used command 1start.",inline = False)
+            embed.add_field(name = "Error using this command. Can\'t make an existance exist.",value = "Used command 1start.",inline = False)
             msg = await ctx.send(embed = embed)
-            await asyncio.sleep(3)
-            embed.clear_fields()
-            embed.add_field(name="Oh you want to register yourself as a new adventurer? Great.",value = "Used command 1start",inline = False)
-            await msg.edit(embed=embed)
-            await asyncio.sleep(5)
-            embed.clear_fields()
-            embed.add_field(name="You are a popular adventurer throughout the Kingdom and you want to start over?",value ="Used command 1start")
-            await msg.edit(embed=embed)
-            await asyncio.sleep(3)
-            embed.clear_fields()
-            embed.add_field(name="Maybe are you looking to prestige. Try 1prestige for that. Hope we can meet again soon.",value = "Try using 1prestige command.",inline = False)
-            await msg.edit(embed=embed)
+            
+    @command(aliases=["pf",])
+    async def profile(self,ctx,user : discord.Member = None):
+        
+        if user == None:
+            user = ctx.author
+            
+        cur.execute(f"SELECT MONEY,LEVEL,XP,BUFFS,WORKPLACE,INCOME,STAMINA,MSTAMINA,HOUSE,VSTREAK,DSTREAK FROM Main WHERE ID={ctx.author.id}")
+        d = cur.fetchall()[0]
+        embed = discord.Embed(title="Profile", description="Do 1houses and 1foods for more options.",color = discord.Color.random())
+        embed.set_author(name=ctx.author.name)
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        embed.add_field(name=f"Money : {d[0]}",value=f"Buffs : {d[3]}",inline=False)
+        embed.add_field(name=f"Stamina : { d[6]}",value=f"Maximum Stamina : {d[7]}",inline=False)
+        exp = xplevel(ctx.author)
+        embed.add_field(name=f"Level:{d[1]}",value=f"Exp:{d[2]}/{exp}",inline=False)
+        embed.add_field(name=f"Home : {d[8]}",value=f"Workplace : {d[4]}",inline=False)
+        embed.add_field(name=f"Daily Streak : {d[10]}",value=f"Voting Streak : {d[9]})
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(BASE(client))
