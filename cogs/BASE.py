3a7500fe1c3720be64e5adac414c451b5923cd17
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-14 20:32:14 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -1,1 +1,20 @@
+import discord,json,os,asyncio,psycopg2
+from discord.ext.commands import *
+from discord_components import Button
+from functions import *
 
+con = psycopg2.connect(os.environ.get("DATABASE_URL"))
+cur = con.cursor()
+
+class BASE(Cog):
+
+    def __init__(self,client):
+
+        self.client = client
+
+    @command()
+    async def eat(self,ctx):
+        pass
+
+def setup(client):
+    client.add_cog(BASE(client))
