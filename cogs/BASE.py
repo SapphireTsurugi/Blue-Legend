c6c7847c1cb14ae41a9f3b2112ec6fb6101a9ef7
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-15 12:16:01 +0530

    update BASE.py

--- cogs/BASE.py
+++ cogs/BASE.py
@@ -16,20 +16,22 @@ class BASE(Cog):
     async def eat(self,ctx,*fdname):
         
         food = " ".join(fdname)
-        cur.execute(f"SELECT FOODSOWNED,STAMINA,MSTAMINA FROM Main WHERE ID={ctx.author.id}")
+        cur.execute(f"SELECT FOODSOWNED,STAMINA,MSTAMINA,HUNGER FROM Main WHERE ID={ctx.author.id}")
         d = cur.fetchall()[0]
         foods = tolist(d[0])
         if food in foods:
             cur.execute(f"SELECT STAMINA FROM Foods WHERE FOOD=\'{food}\'")
             e = cur.fetchall()[0][0]
-            if d[1] == d[2]:
+            if d[1] == d[2] or d[3] > 90:
                 await ctx.send("You are already full.")
-            elif d[1] < d[2]:
+            elif d[1] < d[2] and d[3] < 90:
                 if d[1] + e <= d[2]:
                     f = d[1] + e
                 else:
                     f = d[2]
                 cur.execute(f"UPDATE Main SET STAMINA={f} WHERE ID={ctx.author.id}")
+                con.commit()
+                cur.execute(f"UPDATE Main SET HUNGER=HUNGER+10 WHERE ID={ctx.author.id}")
                 con.commit()
                 await ctx.send("Ate")
         else:
