f9b3e9866f0da69a1433bae48aa0270bb1ad78cd
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-17 13:27:06 +0530

    update functions.py

--- functions.py
+++ functions.py
@@ -5,22 +5,13 @@ from discord_components import Button
 con = psycopg2.connect(os.environ.get("DATABASE_URL"))
 cur = con.cursor()
 
-def xplevel(user):
-    cur.execute(f"SELECT LEVEL FROM Main WHERE ID={user.id}")
-    level = cur.fetchall()[0][0]
+def xplevel(level):
     exp = 2500*level
     if level == 100:
         return 0
         
     else:
         return exp
-
-def buffstolist(buffs):
-    buff = json.loads(buffs)
-    msg = ""
-    for i in buff:
-        msg += str(i)
-        msg += "\n"
         
 def tolist(str2):
     list2 = str2.split(",")
@@ -29,3 +20,11 @@ def tolist(str2):
 def tostring(list2):
     str2 = ",".join(list2)
     return str2
+
+def levelupcheck(xp,level):
+    reqxp = xplevel(level)
+    if xp >= reqxp:
+        return True
+    else:
+        return False
+
