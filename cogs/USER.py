d3cfe638af435534de5875341fe2b789412ae497
Legion04 <86095067+Legion04@users.noreply.github.com>
2021-07-17 11:03:53 +0530

    update USER.py

--- cogs/USER.py
+++ cogs/USER.py
@@ -38,19 +38,17 @@ class USER(Cog):
         if user == None:
             user = ctx.author
             
-        cur.execute(f"SELECT MONEY,LEVEL,XP,BUFFS,WORKPLACE,STAMINA,MSTAMINA,HOUSE,VSTREAK,DSTREAK FROM Main WHERE ID={ctx.author.id};")
+        cur.execute(f"SELECT MONEY,LEVEL,XP,WORKPLACE,STAMINA,MSTAMINA,HOUSE,VSTREAK,DSTREAK FROM Main WHERE ID={ctx.author.id};")
         d = cur.fetchall()[0]
         embed = discord.Embed(title="Profile", description="Do 1houses and 1foods for more options.",color = discord.Color.random())
         embed.set_author(name=ctx.author.name)
         embed.set_thumbnail(url=ctx.author.avatar_url)
-        buffs = buffstolist(d[3])
-        print(buffs)
         embed.add_field(name=f"Money : {d[0]}",value=f"Buffs : {buffs}",inline=False)
-        embed.add_field(name=f"Stamina : { d[5]}",value=f"Maximum Stamina : {d[6]}",inline=False)
+        embed.add_field(name=f"Stamina : { d[4]}",value=f"Maximum Stamina : {d[5]}",inline=False)
         exp = xplevel(ctx.author)
         embed.add_field(name=f"Level : {d[1]}",value=f"Exp : {d[2]}/{exp}",inline=False)
-        embed.add_field(name=f"Home : {d[7]}",value=f"Workplace : {d[4]}",inline=False)
-        embed.add_field(name=f"Daily Streak : {d[9]}",value=f"Voting Streak : {d[8]}")
+        embed.add_field(name=f"Home : {d[6]}",value=f"Workplace : {d[3]}",inline=False)
+        embed.add_field(name=f"Daily Streak : {d[8]}",value=f"Voting Streak : {d[7]}")
         await ctx.send(embed=embed)
         
     @command()
